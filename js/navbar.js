8eab8b3f6da262cb7bc549ab8e2078a5cac896f1
moeinZ <moein459@gmail.com>
2019-10-12 14:26:53 +0330

    Scroll to Top feature implemented

--- js/navbar.js
+++ js/navbar.js
@@ -1,11 +1,14 @@
 $(document).ready(function () {
     const MIN_HEIGHT = 15;
     const NAV_BAR = $('#nav-bar');
+    const SCROLL_TO_TOP = $('.scroll-to-top');
     $(window).scroll(function () {
         if (window.scrollY > MIN_HEIGHT) {
             NAV_BAR.addClass('scrolled-down');
+            SCROLL_TO_TOP.addClass('scrolled-down');
         } else {
             NAV_BAR.removeClass('scrolled-down');
+            SCROLL_TO_TOP.removeClass('scrolled-down');
         }
     });
 });
